52254e9bf3710acc4ab33202a2b51b558938b739
Abdallah Eid <abdallaheidm@gmail.com>
2023-07-01 23:26:47 +0300

    Add Note Post Request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('Hello World!')
+const express = require('express');
+const app = express();
+const Note = require('./models/note')
+
+require('./db/database');
+app.use(express.json());
+
+// MARK: - GET /notes
+
+// app.get('/notes', (req, res) => {
+//     fs.readFile(__dirname + "/notes.json", "utf-8", (err, data) => {
+//         if (err) throw err
+//         res.send(data)
+//     })
+// })
+
+// MARK: - POST /notes
+app.post('/notes', (req, res) => {
+    const note = new Note({
+        note: req.body.note
+      });
+    
+      console.log(note);
+
+      note.save()
+        .then(() => {
+          res.status(201).send(note);
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).send(error);
+        });
+})
+
+app.listen(3000, () => console.log(`App listening on port 3000`))
+
